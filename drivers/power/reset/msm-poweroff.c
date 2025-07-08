78ba1fb68bd2bf96e79b0a087670a602c1c3d08d
duckyduckg <duckyduckg65@gmail.com>
2025-07-08 12:53:29 +0500

    drivers: power: reset: log warm reboot reason
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -304,6 +304,9 @@ static void msm_restart_prepare(const char *cmd)
 	need_warm_reset = true;
 #endif
 
+	if (need_warm_reset || in_panic)
+		pr_info("Forcing a warm reset of the system\n");
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
