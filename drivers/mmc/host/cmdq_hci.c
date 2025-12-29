401f46e9bdeed60e3ecf4c735c1df41eb707cc7e
duckyduckG <duckyduckg65@gmail.com>
2025-12-30 00:17:05 +0500

    drivers: mmc: Implement FIH changes

--- drivers/mmc/host/cmdq_hci.c
+++ drivers/mmc/host/cmdq_hci.c
@@ -868,7 +868,9 @@ static void cmdq_finish_data(struct mmc_host *mmc, unsigned int tag)
 	struct mmc_request *mrq;
 	struct cmdq_host *cq_host = (struct cmdq_host *)mmc_cmdq_private(mmc);
 	int offset = 0;
+#if !defined(CONFIG_FIH_SDM630_SDM660_PROJS)
 	int err = 0;
+#endif
 
 	if (cq_host->offset_changed)
 		offset = CQ_V5_VENDOR_CFG;
@@ -883,6 +885,7 @@ static void cmdq_finish_data(struct mmc_host *mmc, unsigned int tag)
 
 	cmdq_runtime_pm_put(cq_host);
 
+#if !defined(CONFIG_FIH_SDM630_SDM660_PROJS)
 	if (cq_host->ops->crypto_cfg_end) {
 		err = cq_host->ops->crypto_cfg_end(mmc, mrq);
 		if (err) {
@@ -890,6 +893,7 @@ static void cmdq_finish_data(struct mmc_host *mmc, unsigned int tag)
 					mmc_hostname(mmc), err, tag);
 		}
 	}
+#endif
 	if (!(cq_host->caps & CMDQ_CAP_CRYPTO_SUPPORT) &&
 			cq_host->ops->crypto_cfg_reset)
 		cq_host->ops->crypto_cfg_reset(mmc, tag);
@@ -1265,6 +1269,15 @@ static void cmdq_post_req(struct mmc_host *mmc, int tag, int err)
 	cq_host = (struct cmdq_host *)mmc_cmdq_private(mmc);
 	mrq = get_req_by_tag(cq_host, tag);
 	data = mrq->data;
+
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+	if (cq_host->ops->crypto_cfg_end) { 
+		if (cq_host->ops->crypto_cfg_end(mmc, mrq)) { 
+			pr_err("%s: failed to end ice config: err %d tag %d\n", 
+					mmc_hostname(mmc), err, tag); 
+		} 
+	} 
+#endif
 
 	if (data) {
 		data->error = err;
