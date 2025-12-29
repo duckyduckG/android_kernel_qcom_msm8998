795e688406ec8980cd90b90e9bb4b43fc9006671
duckyduckG <duckyduckg65@gmail.com>
2025-12-29 12:59:57 +0500

    drivers: power: reset: Import FIH changes
    Package name: Nokia6.1plus_V4.16C.tar.bz2 | Nokia7plus_V4.16H.tar.bz2
    
    Change-Id: Iece72be31e3b2756f11e0bb5d78f66e039a7497d

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -35,6 +35,10 @@
 #include <soc/qcom/watchdog.h>
 #include <soc/qcom/minidump.h>
 
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+#include "../../../drivers/fih/fih_rere.h"  /* FIH, to support fih reboot command */
+#endif
+
 #define EMERGENCY_DLOAD_MAGIC1    0x322A4F99
 #define EMERGENCY_DLOAD_MAGIC2    0xC67E4350
 #define EMERGENCY_DLOAD_MAGIC3    0x77777777
@@ -109,8 +113,16 @@ struct reset_attribute {
 	static struct reset_attribute reset_attr_##_name = \
 			__ATTR(_name, _mode, _show, _store)
 
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+static int __init oem_dload_set(char *str);
+#endif
+
 module_param_call(download_mode, dload_set, param_get_int,
 			&download_mode, 0644);
+
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+__setup("download_mode=", oem_dload_set);
+#endif
 
 int scm_set_dload_mode(int arg1, int arg2)
 {
@@ -202,6 +214,26 @@ static int dload_set(const char *val, const struct kernel_param *kp)
 
 	return 0;
 }
+
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+static int __init oem_dload_set(char *str)
+{
+    int old_val = download_mode;
+    get_option(&str, &download_mode);
+
+
+    if(download_mode != 0 && download_mode != 1){
+        download_mode = old_val;
+        return -EINVAL;
+    }
+
+    pr_info("******%s check download_mode %d\n", __func__,download_mode);
+	set_dload_mode(download_mode);
+
+    return 1;
+}
+#endif
+
 #else
 static void set_dload_mode(int on)
 {
@@ -217,6 +249,21 @@ static bool get_dload_mode(void)
 {
 	return false;
 }
+#endif
+
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+/* FIH, to support fih apr */
+unsigned int restart_reason_rd(void)
+{
+	return readl(restart_reason);
+}
+
+void restart_reason_wt(unsigned int rere)
+{
+  qpnp_pon_set_restart_reason(rere);
+	__raw_writel(0, restart_reason);
+}
+/* FIH, to support fih apr */
 #endif
 
 static void scm_disable_sdi(void)
@@ -307,7 +354,11 @@ static void msm_restart_prepare(const char *cmd)
 	}
 
 	if (cmd != NULL) {
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+		if (!strncmp(cmd, "bootloader", 10) || !strncmp(cmd, "download", 8)) {
+#else
 		if (!strncmp(cmd, "bootloader", 10)) {
+#endif
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_BOOTLOADER);
 			__raw_writel(0x77665500, restart_reason);
@@ -411,7 +462,9 @@ static void do_msm_restart(enum reboot_mode reboot_mode, const char *cmd)
 		msm_trigger_wdog_bite();
 #endif
 
+#if !defined(CONFIG_FIH_SDM630_SDM660_PROJS)
 	scm_disable_sdi();
+#endif
 	halt_spmi_pmic_arbiter();
 	deassert_ps_hold();
 
