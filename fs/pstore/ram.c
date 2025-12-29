ffc34ac17f518dfa89ad52f4d99f447b150b44d9
duckyduckG <duckyduckg65@gmail.com>
2025-12-29 12:58:28 +0500

    fs: pstore: Import FIH changes

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -45,7 +45,11 @@ module_param(record_size, ulong, 0400);
 MODULE_PARM_DESC(record_size,
 		"size of each dump done on oops/panic");
 
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+static ulong ramoops_console_size = 256*1024UL;  /* FIH: for pstore */
+#else
 static ulong ramoops_console_size = MIN_MEM_SIZE;
+#endif
 module_param_named(console_size, ramoops_console_size, ulong, 0400);
 MODULE_PARM_DESC(console_size, "size of kernel console log");
 
@@ -57,12 +61,20 @@ static ulong ramoops_pmsg_size = MIN_MEM_SIZE;
 module_param_named(pmsg_size, ramoops_pmsg_size, ulong, 0400);
 MODULE_PARM_DESC(pmsg_size, "size of user space message log");
 
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+static ulong mem_address = 0xACB00000;  /* FIH: for pstore */
+#else
 static ulong mem_address;
+#endif
 module_param(mem_address, ulong, 0400);
 MODULE_PARM_DESC(mem_address,
 		"start of reserved RAM used to store oops/panic logs");
 
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+static ulong mem_size = 0x00200000;  /* FIH: for pstore */
+#else
 static ulong mem_size;
+#endif
 module_param(mem_size, ulong, 0400);
 MODULE_PARM_DESC(mem_size,
 		"size of reserved RAM used to store oops/panic logs");
